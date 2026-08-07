9cfd9623218b6117a2f740405cf7329f97a668f5
Monica6788 <kekelee0070@gmail.com>
2026-08-07 12:01:44 +0900

    공유용

---
+++ 02_myBatis/src/main/java/com/kh/mybatis/controller/MemberController.java
@@ -0,0 +1,62 @@
+package com.kh.mybatis.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.mybatis.model.dto.MemberDTO;
+import com.kh.mybatis.model.mapper.MemberMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller	// RestController는 JSON으로 직접 응답할 때
+@RequestMapping("/member")
+@RequiredArgsConstructor // 꼭 필요한 final 필드를 매개변수로 하여 생성자 제공
+public class MemberController {
+	// MemberMapper 주입 (생성자 주입 방식)
+	private final MemberMapper mapper;
+	// final을 붙여야 필수값인 줄을 알고, 할당 못 받을 시 나 이거 할당 못 받았다고! 내놔! 하고 오류 뱉음
+	
+//	public MemberController(MemberMapper mapper) {
+//		this.mapper = mapper;
+//	}
+	
+	/**
+	 * 회원 목록 조회
+	 * URL: [GET] /member/list
+	 * Param: x
+	 * 응답: 회원 목록 페이지(WEB-INF/views/member/list.jsp) 포워딩
+	 */
+	@GetMapping("/list")
+	public String memberList(Model model) {
+		// DB에서 조회된 회원 목록을 request 영역에 저장 (k: memberList)
+		List<MemberDTO> memberList = mapper.findAll();
+		model.addAttribute("memberList", memberList);
+		
+		return "member/list";
+	}
+	
+	/**
+	 * 회원 추가
+	 * URL: [POST] /member/insert
+	 * Param: name(String), email(String), age(int)
+	 * 응답: 회원 목록 페이지로 리다이렉트
+	 */
+	@PostMapping("/insert")
+	public String memberInsert(
+//			@RequestParam(value="name", defaultValue="x") String name
+//			String name, String email, int age
+			// 프로젝트 할 때는 요청 파라미터가 n개 이상이면 DTO를 사용하기로 약속하는 것이 좋음
+			// n은 팀원들과 협의 후 결정...
+			@ModelAttribute MemberDTO member) {
+		
+		
+		return "redirect:/member/list";
+	}
+}
---
+++ 02_myBatis/src/main/webapp/WEB-INF/views/member/list.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html;charsert=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>MyBatis Practice</title>
+</head>
+<body>
+	<h1>회원 목록</h1>
+	
+	<table border="1">
+		<thaed>
+			<tr>
+				<th>ID</th>
+				<th>이름</th>
+				<th>이메일</th>
+				<th>나이</th>
+			</tr>
+		</thaed>
+		<tbody>
+			<%-- JSTL 반복문을 사용하여 조회 결과(memberList)를 한 행씩 출력 --%>
+			<c:if test="${memberList == null}">
+				<td colspan="4">등록된 회원이 없습니다.</td>
+			</c:if>
+			<c:forEach var="member" items="${memberList}">
+				<tr>
+					<td>${member.id}</td>
+					<td>${member.name}</td>			
+					<td>${member.email}</td>
+					<td>${member.age}</td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	
+	</table>
+</body>
+</html>
